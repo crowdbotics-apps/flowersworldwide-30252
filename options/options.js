87c90994dcfa3fe7fc1d3ccb3e40410391b35413
Crowdbotics <team@crowdbotics.com>
2021-09-01 16:18:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "flowersworldwide_30252Identifier",
+  url: "https://flowersworldwide_30252Identifier.botics.co",
+  api: "https://flowersworldwide_30252Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
